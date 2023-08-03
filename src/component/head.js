41948e6852c7b0d3a40208575491cd63f70f9e36
leefrosh <bolajiademola815@gmail.com>
2023-08-03 15:53:29 -0700

    commit

--- src/component/head.js
+++ src/component/head.js
@@ -3,7 +3,7 @@ import sun from "../image/002-sun.png"
 function Head(props) {
     return (
     <div className="head">
-        <h1 className="h1" style={{color: props.gb ? "white" : "#687c9a"}}>devfinder</h1>
+        <h1  style={{color: props.gb ? "white" : "#687c9a"}}>devfinder</h1>
         <span className="dark"   style={{color: props.gb ? "white" : "#687c9a"}}>{props.gb ?  <span>Light</span> : <span>Dark</span>} 
         <img onClick={props.handleclick2} src={props.gb ? sun : moon} alt="" />
         </span>
